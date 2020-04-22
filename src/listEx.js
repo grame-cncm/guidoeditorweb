78c2a09b094d3304c2fac76666b7d887497e28ab
Dominique Fober <fober@grame.fr>
2020-04-22 18:43:24 +0200

    old js implementation removed

--- src/listEx.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-const fs = require("fs");
-const path = require("path");
-const entry = "examples";
-const dirTree = require("directory-tree");
-const filteredTree = dirTree("examples", { extensions: /\.gmn/, normalizePath: true });
-fs.writeFileSync("examples.json", JSON.stringify(filteredTree));
