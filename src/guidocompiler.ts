36a54f9a0996562d797a1491b0e0332780324691
Dominique Fober <fober@grame.fr>
2020-05-27 12:08:06 +0200

    fix session url

--- src/guidocompiler.ts
+++ src/guidocompiler.ts
@@ -201,7 +201,7 @@ class GuidoCompiler {
 					console.log ("editor s option value " + value );
 
 					let iframe = <HTMLIFrameElement>document.getElementById("lxmlcom");
-					iframe.src = "http://libmusicxml.grame.fr/code/?s=" + value;
+					iframe.src = "https://libmusicxml.grame.fr/code/?s=" + value;
 					// iframe.src = "http://localhost:8080/code/?s=" + value;
 					iframe.onload = () => { 
 					let content = iframe.contentWindow.document.getElementById("code");
